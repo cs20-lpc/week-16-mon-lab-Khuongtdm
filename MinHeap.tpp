d79b851ffa456b86874cec3347698f4ca88eb0de
Khuongtdm <152750581+Khuongtdm@users.noreply.github.com>
2026-05-04 23:11:13 -0700

    push

--- MinHeap.tpp
+++ MinHeap.tpp
@@ -1,22 +1,65 @@
+#include <algorithm> 
+using namespace std;
 template <typename T>
 void MinHeap<T>::siftUp(int i) {
     // TODO: Move element at index i upward until heap property restored
+     while (i > 0) {
+        int p = parent(i);
+
+        if (data[i] < data[p]) {
+            swap(data[i], data[p]);
+            i = p;
+        } else {
+            break;
+        }
+    }
 }
 
 template <typename T>
 void MinHeap<T>::siftDown(int i) {
     // TODO: Move element at index i downward until heap property restored
+    int n = size();
+    while (true) {
+
+        int smallest = i;
+        int l = left(i);
+        int r = right(i);
+        if (l < n && data[l] < data[smallest])
+            smallest = l;
+
+        if (r < n && data[r] < data[smallest])
+            smallest = r;
+
+        if (smallest != i) {
+            swap(data[i], data[smallest]);
+            i = smallest;
+        } else {
+            break;
+        }
+    }
+
 }
 
 template <typename T>
 void MinHeap<T>::insert(const T& value) {
     // TO DO: insert a value into the heap
+     data.push_back(value);
+     siftUp(size() - 1);
 }
 
 template <typename T>
 T MinHeap<T>::removeRoot() {
-    T rootValue;
-    // TODO: Implement removing the root element.
+     if (size() == 0)
+        throw runtime_error("Heap is empty");
+
+    T rootValue = data[0];
+
+    data[0] = data.back();
+    data.pop_back();
+
+    if (size() > 0)
+        siftDown(0);
+
     return rootValue;
 }
 
@@ -30,4 +73,8 @@ void MinHeap<T>::removeAt(int index) {
 
     // TODO: Attempt sift-up then 
     // sift-down to restore heap property
+    if (index < size()) {
+        siftUp(index);
+        siftDown(index);
+    }
 }
--- Week16Mon.cpp
+++ Week16Mon.cpp
@@ -10,16 +10,73 @@ void heapsort(vector<T>& arr) {
     // TODO: Insert all elements into heap using sift-down insert from lecture
 
     // TODO: Repeatedly remove root and store back into array
+    for (const T& element : arr) {
+        h.insert(element);
+    }
+    for (int i = 0; i < arr.size(); i++) {
+        arr[i] = h.removeRoot();
+    }
+    
+}
+template <typename T>
+void printHeap(const MinHeap<T>& heap) {
+    const auto& arr = heap.getArray();
+    cout << "Heap Array: [ ";
+    for (const auto& val : arr) {
+        cout << val << " ";
+    }
+    cout << "]" << endl;
 }
 
 int main() {
-    vector<int> arr = {5, 1, 9, 3, 7, 4};
+    try {
+        MinHeap<int> mh;
 
-    heapsort(arr);
+        cout << "--- Testing Insertion ---" << endl;
+        vector<int> values = { 10, 5, 20, 2, 15, 30 };
+        for (int v : values) {
+            cout << "Inserting: " << v << endl;
+            mh.insert(v);
+        }
+        printHeap(mh); // Root should be 2
 
+        cout << "\nTesting removeRoot" << endl;
+        while (!mh.empty()) {
+            cout << "Removed Root: " << mh.removeRoot() << " | ";
+            printHeap(mh);
+        }
+
+        cout << "\n Testing removeAt" << endl;
+        mh.insert(40);
+        mh.insert(10);
+        mh.insert(30);
+        mh.insert(5);
+        printHeap(mh);
+
+        cout << "\nRemoving element at index 1" << endl;
+        mh.removeAt(1);
+        printHeap(mh);
+
+        cout << "\nTesting Exception Handling" << endl;
+        mh.removeRoot(); // Clear heap
+        mh.removeRoot();
+        mh.removeRoot();
+        mh.removeRoot(); // This should trigger error
+
+    }
+    catch (const runtime_error& e) {
+        cout << "Caught expected error: " << e.what() << endl;
+    }
+    cout << "Sorting section to test\n";
+    vector<int> arr = { 5, 1, 9, 3, 7, 4 };
+    cout << "Before sort\n";
     for (int x : arr)
         cout << x << " ";
     cout << endl;
 
-    return 0;
+    heapsort(arr);
+    cout << "after sort\n";
+    for (int x : arr)
+        cout << x << " ";
+    cout << endl;
 }
